4857ecfe67f5eff8ae2eada5a903ca28f4bafb00
Jonathan Kong <jonathankong1996@gmail.com>
2024-05-17 16:14:16 +0800

    edit frontpage layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,11 @@
-import NextLogo from "./NextLogo";
-import SupabaseLogo from "./SupabaseLogo";
+import NextLogo from './NextLogo';
+import SupabaseLogo from './SupabaseLogo';
 
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <div className="flex gap-8 justify-center items-center">
-        <a
+        {/* <a
           href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
           target="_blank"
           rel="noreferrer"
@@ -13,32 +13,18 @@ export default function Header() {
           <SupabaseLogo />
         </a>
         <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+        <a href="https://fatex3x.wixsite.com/calobro" target="_blank" rel="noreferrer">
           <NextLogo />
-        </a>
+        </a> */}
       </div>
       <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
-        </a>
+        <h1 className="font-bold">
+          🥦 Calo<span className="text-orange-500">bro.</span>
+        </h1>{' '}
+        <p className="text-2xl">Enterprise Platform</p>
+        <p className="text-sm"> For business users only.</p>
       </p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
   );
 }
--- components/tutorial/SignUpUserSteps.tsx
+++ components/tutorial/SignUpUserSteps.tsx
@@ -1,20 +1,40 @@
-import Link from "next/link";
-import Step from "./Step";
+import Link from 'next/link';
+import Step from './Step';
 
 export default function SignUpUserSteps() {
   return (
     <ol className="flex flex-col gap-6">
-      <Step title="Sign up your first user">
+      <Step title="Sign up for a company account">
         <p>
-          Head over to the{" "}
+          Head over to the{' '}
           <Link
             href="/login"
             className="font-bold hover:underline text-foreground/80"
           >
             Login
-          </Link>{" "}
-          page and sign up your first user. It's okay if this is just you for
-          now. Your awesome idea will have plenty of users later!
+          </Link>{' '}
+          page and sign up an account for your company. It's okay to upload an
+          easy-to-make recipe. Your awesome recipe will be consumed by many
+          later!
+        </p>
+      </Step>
+      <Step title="Upload your company's food recipe">
+        <p>
+          Fill in the neccessary details and upload your recipe. You can also
+          add a photo of your recipe to make it more appealing.
+        </p>
+      </Step>
+      <Step title="Wait for Calobro's admin approval">
+        <p>
+          Your recipe will be vetted by Calobro's admin. Once approved, your
+          recipe will show up to the public.
+        </p>
+      </Step>
+      <Step title="Get rewarded with incentives and real customers">
+        <p>
+          If your recipe gains traction, you will be rewarded with incentives
+          and real customers. You can also get feedback from your customers to
+          improve your recipe.
         </p>
       </Step>
     </ol>
